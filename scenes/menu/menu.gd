7d182b46bad7987c6b9e002518b26f459ff0bb0d
Alex Wu <adwu@pm.me>
2024-11-06 18:47:53 -0800

    chore: Updates to Godot 4.3 and various GH Actions fixes (#1)
    ### Notable changes
    
    - Adds `justfile`
    - Adds `gdformat` and `gdlint` basic config files
    - Updates Godot to 4.3
    - Updates GitHub pages flow

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -7,8 +7,8 @@ extends Control
 func _ready():
 	# needed for gamepads to work
 	btn_play.grab_focus()
-	if OS.has_feature('web'):
-		btn_exit.queue_free() # exit button dosn't make sense on HTML5
+	if OS.has_feature("web"):
+		btn_exit.queue_free()  # exit button dosn't make sense on HTML5
 
 
 func _on_PlayButton_pressed() -> void:
@@ -17,10 +17,7 @@ func _on_PlayButton_pressed() -> void:
 		"a_number": 10,
 		"a_string": "Ciao!",
 		"an_array": [1, 2, 3, 4],
-		"a_dict": {
-			"name": "test",
-			"val": 15
-		},
+		"a_dict": {"name": "test", "val": 15},
 	}
 	Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
 
@@ -29,9 +26,7 @@ func _on_ExitButton_pressed() -> void:
 	# gently shutdown the game
 	var transitions = get_node_or_null("/root/Transitions")
 	if transitions:
-		transitions.fade_in({
-			'show_progress_bar': false
-		})
+		transitions.fade_in({"show_progress_bar": false})
 		await transitions.anim.animation_finished
 		await get_tree().create_timer(0.3).timeout
 	get_tree().quit()
